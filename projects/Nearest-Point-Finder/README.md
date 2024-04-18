7e46670f81dc9b81b9e0a73f44ff9bdbaecf6fda
novaepitech <mateo.cabrera@epitech.eu>
2024-04-18 23:42:37 +0200

    feat: implemented cell partitioning algorithm for way better performance

--- projects/Nearest-Point-Finder/README.md
+++ projects/Nearest-Point-Finder/README.md
@@ -3,3 +3,9 @@
 The goal of this project is to create a web application that draws a large number of points at random positions within a canvas.
 
 As the user moves their mouse cursor, the application calculates in real-time which point is closest to the cursor.
+
+## [Live Website](https://novaepitech.github.io/projects/Nearest-Point-Finder/)
+
+## Demo :
+
+![](https://github.com/novaepitech/novaepitech.github.io/blob/main/projects/Nearest-Point-Finder/live-demo.gif)
--- projects/Nearest-Point-Finder/script.js
+++ projects/Nearest-Point-Finder/script.js
@@ -5,12 +5,18 @@
 ** draw points on a canvas and highlight the nearest point to the cursor
 */
 
+class Cell {
+    constructor() {
+        this.points = [];
+    }
+}
+
 function generate_random_point(width, height)
 {
     const x = Math.random() * width;
     const y = Math.random() * height;
 
-    return { x, y };
+    return [x, y];
 }
 
 function generate_points(num_points, width, height)
@@ -26,7 +32,7 @@ function draw_points(points, buffer_context, canvas_context, buffer)
 {
     buffer_context.fillStyle = 'black';
     for (let i = 0; i < points.length; i++) {
-        const { x, y } = points[i];
+        const [x, y] = points[i];
 
         buffer_context.beginPath();
         buffer_context.arc(x, y, 1, 0, 2 * Math.PI);
@@ -35,38 +41,99 @@ function draw_points(points, buffer_context, canvas_context, buffer)
     canvas_context.drawImage(buffer, 0, 0);
 }
 
-function find_nearest_point(cursor_x, cursor_y, points)
+function create_grid(points, width, height, num_cells)
 {
-    let min_distance = Infinity;
+    const grid = [];
+    const cell_size = Math.max(width, height) / num_cells;
+
+    for (let i = 0; i < num_cells; i++) {
+        grid[i] = [];
+        for (let j = 0; j < num_cells; j++) {
+            grid[i][j] = new Cell();
+        }
+    }
+    for (let point of points) {
+        const [x, y] = point;
+        const cell_x = Math.floor(x / cell_size);
+        const cell_y = Math.floor(y / cell_size);
+
+        grid[cell_x][cell_y].points.push(point);
+    }
+    return { grid, cell_size };
+}
+
+function get_cell_coordinates(point, cell_size)
+{
+    const [x, y] = point;
+    const cell_x = Math.floor(x / cell_size);
+    const cell_y = Math.floor(y / cell_size);
+
+    return [cell_x, cell_y];
+}
+
+function get_surrounding_cells(grid, cell_x, cell_y)
+{
+    const cells = [];
+
+    for (let i = Math.max(0, cell_x - 1); i <= Math.min(grid.length - 1, cell_x + 1); i++) {
+        for (let j = Math.max(0, cell_y - 1); j <= Math.min(grid[i].length - 1, cell_y + 1); j++) {
+            cells.push(grid[i][j]);
+        }
+    }
+    return cells;
+}
+
+function find_nearest_point_in_cells(cells, point)
+{
     let nearest_point = null;
+    let nearest_distance = Infinity;
 
-    for (let i = 0; i < points.length; i++) {
-        const { x, y } = points[i];
-        const distance = Math.hypot(x - cursor_x, y - cursor_y);
+    for (let cell of cells) {
+        for (let cell_point of cell.points) {
+            const distance = distance_sq(cell_point, point);
 
-        if (distance < min_distance) {
-            min_distance = distance;
-            nearest_point = points[i];
+            if (distance < nearest_distance) {
+                nearest_distance = distance;
+                nearest_point = cell_point;
+            }
         }
     }
     return nearest_point;
 }
 
-function handle_mousemove(event, points, canvas, buffer, canvas_context)
+function find_nearest_point(grid, cell_size, point)
+{
+    const [cell_x, cell_y] = get_cell_coordinates(point, cell_size);
+    const cells = get_surrounding_cells(grid, cell_x, cell_y);
+    const nearest_point = find_nearest_point_in_cells(cells, point);
+
+    return nearest_point;
+}
+
+function distance_sq(pointA, pointB)
+{
+    const dx = pointA[0] - pointB[0];
+    const dy = pointA[1] - pointB[1];
+
+    return dx * dx + dy * dy;
+}
+
+function handle_mousemove(event, canvas, buffer, canvas_context, grid, cell_size)
 {
     const rect = canvas.getBoundingClientRect();
     const cursor_x = event.clientX - rect.left;
     const cursor_y = event.clientY - rect.top;
-    const nearest_point = find_nearest_point(cursor_x, cursor_y, points);
+    console.time('find_nearest_point')
+    const nearest_point = find_nearest_point(grid, cell_size, [cursor_x, cursor_y]);
+    console.timeEnd('find_nearest_point')
 
     canvas_context.clearRect(0, 0, canvas.width, canvas.height);
     canvas_context.drawImage(buffer, 0, 0);
     canvas_context.fillStyle = 'red';
     canvas_context.beginPath();
-    canvas_context.arc(nearest_point.x, nearest_point.y, 3, 0, 2 * Math.PI);
+    canvas_context.arc(nearest_point[0], nearest_point[1], 3, 0, 2 * Math.PI);
     canvas_context.fill();
 }
-
 
 function main()
 {
@@ -79,9 +146,10 @@ function main()
     buffer.height = canvas.height;
     const buffer_context = buffer.getContext('2d');
     const points = generate_points(num_points, canvas.width, canvas.height);
+    const { grid, cell_size } = create_grid(points, canvas.width, canvas.height, num_points / 10000);
 
     draw_points(points, buffer_context, canvas_context, buffer);
-    canvas.addEventListener('mousemove', (event) => handle_mousemove(event, points, canvas, buffer, canvas_context));
+    canvas.addEventListener('mousemove', (event) => handle_mousemove(event, canvas, buffer, canvas_context, grid, cell_size));
 }
 
 main();
